7d7313f1838051d65b29eef6db2da971456aaccd
匿名なかむ <yuya115.m2@gmail.com>
2019-11-12 12:05:02 +0900

    firstVCの作成

---
+++ Guide_sign_mapping/FirstViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FirstViewController.swift
+//  Guide_sign_mapping
+//
+//  Created by yuya on 2019/11/12.
+//  Copyright © 2019 yuya. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
