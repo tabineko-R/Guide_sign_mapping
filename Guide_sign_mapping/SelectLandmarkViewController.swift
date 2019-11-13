bd9ada0bb37b0d4f449a281754aebd2b83e1056b
匿名なかむ <yuya115.m2@gmail.com>
2019-11-13 17:53:12 +0900

    表示オブジェクト選択画面用のVCを作成

---
+++ Guide_sign_mapping/SelectLandmarkViewController.swift
@@ -0,0 +1,47 @@
+//
+//  SelectLandmarkViewController.swift
+//  Guide_sign_mapping
+//
+//  Created by yuya on 2019/11/13.
+//  Copyright © 2019 yuya. All rights reserved.
+//
+
+import UIKit
+
+class SelectLandmarkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // ①セグエ実行前処理
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+             // ②遷移先ViewCntrollerの取得
+             let nextView = segue.destination as! LandmarkViewController
+             // ③Segueの識別子確認
+             switch segue.identifier {
+                //④値の設定
+                 case "1" :
+                     nextView.number = 1
+                 case "2" :
+                     nextView.number = 2
+                 case "3" :
+                     nextView.number = 3
+                 default:
+                     break
+             }
+         }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
