8e0ba0c4f0b9661c323eaf41e9cb111ff040cbf8
匿名なかむ <yuya115.m2@gmail.com>
2020-01-08 21:55:02 +0900

    横スクロール画面　実装 参考（https://techlife.cookpad.com/entry/2019/08/16/090000）

---
+++ Guide_sign_mapping/First/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AViewController.swift
+//  Guide_sign_mapping
+//
+//  Created by yuya on 2020/01/08.
+//  Copyright © 2020 yuya. All rights reserved.
+//
+
+import UIKit
+
+class AViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
